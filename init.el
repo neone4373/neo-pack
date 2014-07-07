2f432049d57f0ecfdc4185093f1f84182902e0a4
Neo Ellison <neo@signafire.com>
2014-07-07 18:02:18 -0400

    clean just a little

--- init.el
+++ init.el
@@ -29,11 +29,8 @@
 ;; scroll one line at a time (less "jumpy" than defaults)
 
 (setq mouse-wheel-scroll-amount '(4 ((shift) . 4))) ;; one line at a time
-
 (setq mouse-wheel-progressive-speed nil) ;; don't accelerate scrolling
-
 (setq mouse-wheel-follow-mouse 't) ;; scroll window under mouse
-
 (setq scroll-step 1) ;; keyboard scroll one line at a time
 
 ;; Set default window width and height
